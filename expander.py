47bc2ce4568393c2b2f420a4e08bd5fc53812b78
Gareth Tan <garethwtan@gmail.com>
2014-05-04 19:32:28 -0400

    Add at-root support.

--- expander.py
+++ expander.py
@@ -7,14 +7,26 @@ class SassexpanderCommand(sublime_plugin.TextCommand):
 
     curpos = self.view.sel()[0].begin()
     self.selector_machine(curpos)
-    selector_array = map(self.process_selector, self.selectors)[::-1]
+    self.process_at_root()
+
+    selector_array = filter(lambda x : not re.search('@(for|each|while|if)', x), self.selectors)
+    selector_array = map(self.process_selector, selector_array)
+    ### Past this point are mostly differences in formatting
+    # If loop directive information must be retained,
+    # modify the filter above
+
+    # generate_expanded takes an array of arrays and joins them together
+    # e.g. [['.hello', '.there'], ['.one', '.two']]
+    # gives ['.hello .one', '.hello .two', '.there .one', '.there .two']
     self.generate_expanded(selector_array)
-    print self.selectors
+    status = self.strip_whitespace(', '.join(self.selectors))
+    sublime.message_dialog(status)
 
   def selector_machine(self, cursorpos):
     position = self.push_next_selector(cursorpos)
     while position >= 0:
       position = self.push_next_selector(position)
+    self.selectors = self.selectors[::-1]
 
   def push_next_selector(self, startpos):
     bracket_counter = 0
@@ -50,7 +62,7 @@ class SassexpanderCommand(sublime_plugin.TextCommand):
 
   def gather_selector(self, openposition):
     selector = ''
-    selectorposition = openposition - 1
+    selectorposition = openposition
     char = self.view.substr(selectorposition)
 
     while char != ';' and char != '{' and char != '\n' and (char != '/' and self.lookahead(selectorposition) != '*') and selectorposition >= 0:
@@ -94,21 +106,65 @@ class SassexpanderCommand(sublime_plugin.TextCommand):
 
     return [False, savedpos]
 
-  def parse_at_root(self, selector):
-    pass
+  def process_at_root(self):
+    selectors = self.selectors
+    # Group 1: with/without
+    # Group 2: space-separated list of with/without directives
+    AT_ROOT_RE = re.compile(r'@at-root\s*(?:\((with|without)\s*:\s*((?:\w+\s?)+)\))?\s*')
+    at_root_index = None
+    at_root = dict() # only the final at-root is important
+    for index, selector in enumerate(selectors):
+      at_root_match = re.search(AT_ROOT_RE, selector)
+      if at_root_match:
+        at_root_index = index
+        at_root['exclusion'] = at_root_match.group(1)
+        if at_root_match.group(2):
+          at_root['directives'] = at_root_match.group(2).split(' ')
+        else:
+          at_root['directives'] = []
+
+    if not at_root_index:
+      return
+
+    directive_re = re.compile('@(' + '|'.join(at_root['directives']) +')')
+
+    # @at-root has two special values, 'all' and 'rule'
+    if at_root['exclusion'] == 'without' and 'all' in at_root['directives']:
+      selectors[at_root_index] = re.sub(AT_ROOT_RE, '', selectors[at_root_index])
+      self.selectors = selectors[at_root_index:] #discard everything
+      return
+    elif at_root['exclusion'] == 'with' and 'rule' in at_root['directives']:
+      filter_func = lambda x: re.match(directive_re, x) or not re.match('@', x) #keep listed directives and all rules
+    elif at_root['exclusion'] == 'with':
+      filter_func = lambda x: re.match(directive_re, x) #keep only listed directives
+    elif at_root['exclusion'] == None or at_root['exclusion'] == 'without' and 'rule' in at_root['directives']:
+      filter_func = lambda x: re.match('@', x) #just keep directives
+    elif at_root['exclusion'] == 'without':
+      filter_func = lambda x: re.match('@', x) and not re.match(directive_re, x) #keep directives but not those listed
+
+    allowed_directives = filter(filter_func, selectors[:at_root_index])
+    selectors[at_root_index] = re.sub(AT_ROOT_RE, '', selectors[at_root_index])
+    self.selectors = allowed_directives + selectors[at_root_index:]
 
   def process_selector(self, selector):
-    return selector.split(',')
+    split = []
+    if re.search('@', selector):
+      split = [selector]
+    else:
+      split = selector.split(',')
+    return split
 
   # selector array goes forward
-  # TODO: deal with case of directive
   def generate_expanded(self, selector_array):
 
     def comma_reducer(array, following_array):
       results = []
       for selector in array:
         for sel in following_array:
-          results.append(selector + ' ' + sel)
+          if re.search('&', sel):
+            results.append(sel.replace('&', selector))
+          else:
+            results.append(selector + ' ' + sel)
       return results
 
     self.selectors = reduce(comma_reducer, selector_array)
